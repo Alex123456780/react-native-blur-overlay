f309e2750e522a4b0ed0b6c18a2103104f53f8aa
gHashTag <123>
2019-03-14 15:45:41 +0300

    make code simpler

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-import {View, Platform, NativeModules, requireNativeComponent, StyleSheet, TouchableWithoutFeedback,Animated} from 'react-native';
+import {
+    View,
+    Platform,
+    // NativeModules,
+    requireNativeComponent,
+    StyleSheet,
+    // TouchableWithoutFeedback,
+    // Animated
+} from 'react-native';
 import React, {Component,} from 'react';
-import PropTypes from 'prop-types'
-var emitter = require('tiny-emitter/instance');
+import PropTypes from 'prop-types';
+// var emitter = require('tiny-emitter/instance');
 
-const {SajjadBlurOverlay} = NativeModules;
-var iface = {
+// const {SajjadBlurOverlay} = NativeModules;
+const iface = {
     name: 'BlurView',
     propTypes: {
         ...View.propTypes,
@@ -15,74 +23,83 @@ var iface = {
         vibrant: PropTypes.bool,
     }
 };
-var RCTSajjadBlurOverlay = Platform.select({
+
+const RCTSajjadBlurOverlay = Platform.select({
   ios: () => requireNativeComponent('SajjadBlurOverlay', iface),
   android: () => requireNativeComponent('RCTSajjadBlurOverlay', iface),
 })();
-export default class BlurOverlay extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          showBlurOverlay: false,
-          fadeIn: new Animated.Value(0),
-        }
-        this._openOverlay = this.openOverlay.bind(this)
-        this._closeOverlay = this.closeOverlay.bind(this)
-    }
-    openOverlay(){
-       this.setState({
-           showBlurOverlay: true,
-           fadeIn: new Animated.Value(0),
-       }, () => {
-            Animated.timing(
-                this.state.fadeIn,
-                {
-                    toValue: 1,
-                    duration: 500,
-                    useNativeDriver: true
-                }
-            ).start();
-       })
-    }
-    closeOverlay(){
-        Animated.timing(
-            this.state.fadeIn,
-            {
-                toValue: 0,
-                duration: 500,
-                useNativeDriver: true
-            }
-        ).start(()=>this.setState({showBlurOverlay: false}));
-    }
-    componentDidMount(){
-        emitter.on('drawer-open',this._openOverlay);
-        emitter.on('drawer-close',this._closeOverlay);
-    }
-    componentWillUnmount(){
-        emitter.off('drawer-open',this._openOverlay);
-        emitter.off('drawer-close',this._closeOverlay);
-    }
+
+export default class BlurOverlay extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       showBlurOverlay: true,
+    //     //   fadeIn: new Animated.Value(0),
+    //     }
+    //     // this._openOverlay = this.openOverlay.bind(this)
+    //     // this._closeOverlay = this.closeOverlay.bind(this)
+    // }
+    // openOverlay = () => {
+    //    this.setState({
+    //        showBlurOverlay: true,
+    //     //    fadeIn: new Animated.Value(0),
+    //    }
+    // //    , () => {
+    // //         Animated.timing(
+    // //             this.state.fadeIn,
+    // //             {
+    // //                 toValue: 1,
+    // //                 duration: 500,
+    // //                 useNativeDriver: true
+    // //             }
+    // //         ).start();
+    // //    }
+    //    )
+    // }
+    // closeOverlay = () => {
+    //     // this.setState({showBlurOverlay: false})
+    //     // Animated.timing(
+    //     //     this.state.fadeIn,
+    //     //     {
+    //     //         toValue: 0,
+    //     //         duration: 500,
+    //     //         useNativeDriver: true
+    //     //     }
+    //     // ).start(()=>this.setState({showBlurOverlay: false}));
+    // }
+    // componentDidMount(){
+    //     emitter.on('drawer-open',this._openOverlay);
+    //     emitter.on('drawer-close',this._closeOverlay);
+    // }
+    // componentWillUnmount(){
+    //     emitter.off('drawer-open',this._openOverlay);
+    //     emitter.off('drawer-close',this._closeOverlay);
+    // }
 
     render() {
-        const { children } = this.props;
+        // const { children } = this.props;
         return (
-            this.state.showBlurOverlay ?
-            <Animated.View style={[ {opacity: this.state.fadeIn},styles.style]}>
-            <TouchableWithoutFeedback style={styles.style} onPress={this._closeOverlay}>
+            // this.state.showBlurOverlay ?
+        // <Animated.View style={[ {opacity: this.state.fadeIn},styles.style]}>
+        // <View style={[ {opacity: this.state.fadeIn},styles.style]}>
+        <View style={styles.style}>
+            {/* <TouchableWithoutFeedback style={styles.style} onPress={this._closeOverlay}> */}
                 <RCTSajjadBlurOverlay {...this.props} style={[this.props.customStyles,styles.style]}>
                 <View style={[this.props.customStyles,styles.style]}>
-                    {children}
+                    null
                 </View>
-                </RCTSajjadBlurOverlay>
-            </TouchableWithoutFeedback>
-            </Animated.View> :
-                null
+            </RCTSajjadBlurOverlay>
+        {/* </TouchableWithoutFeedback> */}
+        </View>
+        // </Animated.View>
+            // :
+            //     null
         );
     }
 }
 
 const styles = StyleSheet.create({
-    style: {
+    container: {
         position: 'absolute',
         flex: 1,
         left: 0,
@@ -95,9 +112,9 @@ const styles = StyleSheet.create({
         zIndex: 999,
     },
 });
-export function openOverlay() {
-    emitter.emit('drawer-open');
-}
-export function closeOverlay() {
-    emitter.emit('drawer-close');
-}
+// export function openOverlay() {
+//     emitter.emit('drawer-open');
+// }
+// export function closeOverlay() {
+//     emitter.emit('drawer-close');
+// }
